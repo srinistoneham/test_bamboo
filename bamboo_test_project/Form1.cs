824fb25cb07e2039774da56d05afcf61d44c5735
Srini B <srbale@gmail.com>
2012-12-18 17:44:11 -0500

    first version
    first version

---
+++ bamboo_test_project/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace bamboo_test_project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Version 1
+            string name = "Version 1";
+
+
+        }
+    }
+}
