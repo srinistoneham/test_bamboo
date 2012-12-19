55aad607d29666fae928bcd49759507605095041
Srini B <srbale@gmail.com>
2012-12-19 16:09:21 -0500

    gfg
    gg

--- bamboo_test_project/Form1.cs
+++ bamboo_test_project/Form1.cs
@@ -19,7 +19,7 @@ namespace bamboo_test_project
         private void Form1_Load(object sender, EventArgs e)
         {
             // Version 4
-            string name = "Version 7ggddddddddddkljnmmnmnkdgdfgjkxvjddgxaasdgfsdxrgxxxxx";
+            string name = "Version as";
             MessageBox.Show(name);
 
 
