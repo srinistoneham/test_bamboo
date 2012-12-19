0afb2c898d8de84f49faf911f1dc82ef242cdccb
Srini B <srbale@gmail.com>
2012-12-19 14:30:21 -0500

    sad
    asd

--- bamboo_test_project/Form1.cs
+++ bamboo_test_project/Form1.cs
@@ -19,7 +19,7 @@ namespace bamboo_test_project
         private void Form1_Load(object sender, EventArgs e)
         {
             // Version 4
-            string name = "Version 7gggxxxxxxx";
+            string name = "Version 7ggddddddddddddgxxxxxxx";
             MessageBox.Show(name);
 
 
