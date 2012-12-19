eae507313441b7c6966874244ce6a7f9cb9b2331
Srini B <srbale@gmail.com>
2012-12-19 14:24:19 -0500

    we
    we

--- bamboo_test_project/Form1.cs
+++ bamboo_test_project/Form1.cs
@@ -19,7 +19,7 @@ namespace bamboo_test_project
         private void Form1_Load(object sender, EventArgs e)
         {
             // Version 4
-            string name = "Version 6";
+            string name = "Version 76";
             MessageBox.Show(name);
 
 
